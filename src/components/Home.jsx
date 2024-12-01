0bd383ef49cd50c5bf05fb71e6109765ae3eb379
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-12-01 19:03:52 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
+import ShowMore from "../assets/plus.png";
+import ShowLess from "../assets/minus.png";
+import cart from "../assets/cart-outline.svg";
+import addedCart from "../assets/cart.svg";
+import { NavLink } from "react-router-dom";
+import TestImage from "../assets/pouch.png"
 
 function HomePage() {
   const [isDarkMode, setIsdarkMode] = useState(false);
+  const [addedCarts, setAddedCart] = useState(Array(8).fill(false));
+
+  const toggleCart = (index) => {
+    const updatedCart = [...addedCarts]; // Create a copy of the array
+    updatedCart[index] = !updatedCart[index]; // Toggle the specific product's liked status
+    setAddedCart(updatedCart); // Update the state
+  };
   const toggleDarkMode = () => {
     setIsdarkMode(!isDarkMode);
+  };
+  
+  const [seeMore, setSeeMore] = useState([ false, false, false, false, false, false, ]);
+
+  const toggleSection = (index) => {
+    setSeeMore((prevSeeMore) =>
+      prevSeeMore.map((item, i) => (i === index ? !item : item))
+    );
   };
   return (
     <div className={`home ${isDarkMode ? "dark" : ""}`}>
@@ -40,6 +61,200 @@ function HomePage() {
             </button>
           </div>
           <div className="about-img"></div>
+        </div>
+        <div className={`trending ${isDarkMode ? "dark" : ""}`}>
+        <h2>Trending Products</h2>
+        <h1>
+          Our new arrivals are built to withstand your activities while keeping
+          your home powered always
+        </h1>
+        <div className="products-box">
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product" >
+                  <h2>Carabiner Set</h2>
+                </NavLink>
+                <h3>
+                  $24.54 <del>$33.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(0)}
+                  src={addedCarts[0] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Off-Road Shoulder Bag</h2>
+                </NavLink>
+                <h3>
+                  $34.54 <del>$43.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(1)}
+                  src={addedCarts[1] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Cozy Indoor Sandals</h2>
+                </NavLink>
+                <h3>
+                  $40.54 <del>$45.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(2)}
+                  src={addedCarts[2] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Thermo Cup</h2>
+                </NavLink>
+                <h3>
+                  $14.54 <del>$23.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(3)}
+                  src={addedCarts[3] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Outdoor Vest</h2>
+                </NavLink>
+                <h3>
+                  $34.54 <del>$43.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(4)}
+                  src={addedCarts[4] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Face Cap</h2>
+                </NavLink>
+                <h3>
+                  $10.54 <del>$15.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(5)}
+                  src={addedCarts[5] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Fuzzy Cardigan</h2>
+                </NavLink>
+                <h3>
+                  $34.54 <del>$43.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(6)}
+                  src={addedCarts[6] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-image">
+              <img src={TestImage} alt="" />
+            </div>
+            <div className="product-info">
+              <aside>
+                <NavLink to="/product">
+                  <h2>Swivel Chair</h2>
+                </NavLink>
+                <h3>
+                  $54.54 <del>$63.53</del>
+                </h3>
+              </aside>
+              <span id="info-cart">
+                {" "}
+                <img
+                  onClick={() => toggleCart(7)}
+                  src={addedCarts[7] ? addedCart : cart}
+                  alt="cart icon"
+                />{" "}
+              </span>
+            </div>
+          </div>
+        </div>
+        <button className={`more-btn ${isDarkMode ? "dark" : ""}`}>See More</button>
         </div>
         <div className="testimonials">
           <div className={`testimonial-head ${isDarkMode ? "dark" : ""}`}>
@@ -187,6 +402,95 @@ function HomePage() {
             </div>
             <button className={`${isDarkMode ? "dark" : ""}`} type="submit">Submit</button>
           </form>
+        </div>
+        <div className="faq-box">
+        <div className={`faq ${isDarkMode ? "dark" : ""}`}>
+            <h1>Frequently Asked Questions</h1>
+          <section>
+            <header>
+              <h1> how long does it take to install a solar panel? </h1>
+              <button
+                onClick={() => toggleSection(1)}
+                className={seeMore[1] ? "active" : ""}
+              >
+                <img src={seeMore[1] ? ShowLess : ShowMore} alt="" />
+              </button>
+            </header>
+            <main className={`more ${seeMore[1] ? "active" : ""}`}>
+              <p className={`${isDarkMode ? "dark" : ""}`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero id saepe nobis fugiat quisquam dolores animi sequi illo nisi! Iusto cumque labore ullam ad at quo voluptate tempora delectus? Dolorem!
+                Animi nemo, veniam at vel, delectus ipsum quae eaque id nihil quam accusamus.
+              </p>
+            </main>
+          </section>
+          <section>
+            <header>
+              <h1> are there any government incentives for solar panels? </h1>
+              <button
+                onClick={() => toggleSection(2)}
+                className={seeMore[2] ? "active" : ""}
+              >
+                <img src={seeMore[2] ? ShowLess : ShowMore} alt="" />
+              </button>
+            </header>
+            <main className={`more ${seeMore[2] ? "active" : ""}`}>
+              <p className={`${isDarkMode ? "dark" : ""}`}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla reiciendis minus temporibus molestiae eaque debitis dolorum totam dolore vero velit veniam molestias, voluptas, corporis eveniet soluta libero voluptatum iure! Distinctio.
+                Molestias accusantium, itaque perspiciatis perferendis quibusdam veniam accusamus nostrum eligendi.
+              </p>
+            </main>
+          </section>
+          <section>
+            <header>
+              <h1> how much electricity can a solar panel system generate? </h1>
+              <button
+                onClick={() => toggleSection(3)}
+                className={seeMore[3] ? "active" : ""}
+              >
+                <img src={seeMore[3] ? ShowLess : ShowMore} alt="" />
+              </button>
+            </header>
+            <main className={`more ${seeMore[3] ? "active" : ""}`}>
+              <p className={`${isDarkMode ? "dark" : ""}`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero neque nihil quidem accusantium corrupti provident? Aperiam officiis, architecto obcaecati dolorem sapiente, necessitatibus ex aspernatur nemo nobis iste molestias corrupti.
+                Reprehenderit consequatur, placeat quidem rem recusandae quibusdam adipisci velit.
+              </p>
+            </main>
+          </section>
+          <section>
+            <header>
+              <h1> do solar panels require maintenance? </h1>
+              <button
+                onClick={() => toggleSection(4)}
+                className={seeMore[4] ? "active" : ""}
+              >
+                <img src={seeMore[4] ? ShowLess : ShowMore} alt="" />
+              </button>
+            </header>
+            <main className={`more ${seeMore[4] ? "active" : ""}`}>
+              <p className={`${isDarkMode ? "dark" : ""}`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam aliquid vel inventore deleniti illo nihil minima deserunt, atque odio architecto eligendi quisquam suscipit repellendus culpa ab repellat maxime sunt.
+                Reiciendis unde minus exercitationem voluptas sapiente.
+              </p>
+            </main>
+          </section>
+          <section>
+            <header>
+              <h1> can i setup a recurring payment? </h1>
+              <button
+                onClick={() => toggleSection(5)}
+                className={seeMore[5] ? "active" : ""}
+              >
+                <img src={seeMore[5] ? ShowLess : ShowMore} alt="" />
+              </button>
+            </header>
+            <main className={`more ${seeMore[5] ? "active" : ""}`}>
+              <p className={`${isDarkMode ? "dark" : ""}`}>
+                Yes, we offer the option to set up recurring payment. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit error laboriosam saepe nihil quis consequatur sunt debitis. Incidunt praesentium velit delectus minima tempora magni, consequatur ex itaque fugit accusantium sed.
+              </p>
+            </main>
+          </section>
+        </div>
         </div>
       </main>
       <footer>
